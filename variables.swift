d45190f6dcebbae9b796f328d5eeb0577a279c48
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-08-05 07:33:33 -0400

    NSMutableArrays

--- variables.swift
+++ variables.swift
@@ -9,3 +9,31 @@ var names = [myName, yourName]
 names = ["Bla"]
 names.append("Bar")
 names.append("Baz")
+
+//By assigning the value of foo to foo2 we are copying the value of foo into foo2. Your just creating a new instance of it.
+let foo = "Foo"
+let foo2 = foo
+foo2 = "Foo 2"
+foo 
+foo2
+
+let moreNames = ["Foo", "Bar"]
+var copy = moreNames
+copy.append("Baz")
+moreNames
+copy
+
+let oldArray = NSMutableArray(array : ["Foo", "Bar"])
+oldArray.add("Baz")
+var newArray = oldArray
+newArray.add("Qux")
+oldArray
+newArray
+
+let someNames = NSMutablesArray(array : ["Foo", "Bar"])
+func changeTheArray(_ arrange: NSArray){
+    let copy = array as! NSMutableArray
+    copy.add("Baz")
+}
+changeTheArray(someNames)
+someNames //someNames has been internally mutated
