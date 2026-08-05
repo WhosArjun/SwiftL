d93cc5bc3ed984422a775b94703352c0d0a66fd4
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-08-05 07:18:35 -0400

    var and let

--- variables.swift
+++ variables.swift
@@ -3,3 +3,9 @@ import Foundation
 //let variables cannot be assigned to again
 let myName = "Vandad" 
 let yourName = "Foo"
+
+//var variables can be assigned to again
+var names = [myName, yourName]
+names = ["Bla"]
+names.append("Bar")
+names.append("Baz")
